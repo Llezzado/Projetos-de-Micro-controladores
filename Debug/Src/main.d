8df0fbb98c3d343e5ddd52bbb3786c519cb44a6b
Juan_ <97142963+Llezzado@users.noreply.github.com>
2024-09-04 23:57:21 -0300

    Adição da pratica 1

---
+++ Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ /home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/core_cm3.h \
+ /home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/cmsis_version.h \
+ /home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/cmsis_compiler.h \
+ /home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/cmsis_gcc.h \
+ /home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+/home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+/home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+/home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/core_cm3.h:
+/home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/cmsis_version.h:
+/home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/cmsis_compiler.h:
+/home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Include/cmsis_gcc.h:
+/home/juan/STM32CubeIDE/workspace_1.15.1/atv\ micro\ 1\ \ enxuto/F1_Header/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
